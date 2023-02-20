312cd199a18b2a9ed766a27d78478b6b39de40ea
Richard Marto <iam_rmarto@idcontractors.net>
2023-02-20 16:19:16 -0300

    Adds rudimentary api for posting

--- src/main/scala/socialmedia/adapters/service/UserRegistrationServiceImpl.scala
+++ src/main/scala/socialmedia/adapters/service/UserRegistrationServiceImpl.scala
@@ -3,11 +3,12 @@ package socialmedia.adapters.service
 import akka.actor.typed.ActorSystem
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.grpc.GrpcServiceException
+import akka.http.scaladsl.model.DateTime
 import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.{Logger, LoggerFactory}
-import socialmedia.core.user.{UserRegistration}
-import socialmedia.proto.{RegisterUserRequest, User, UserRegistrationService}
+import socialmedia.core.user.UserRegistration
+import socialmedia.proto.{Post, PostPostRequest, RegisterUserRequest, User, UserRegistrationService}
 
 import scala.concurrent.{Future, TimeoutException}
 
@@ -31,16 +32,26 @@ class UserRegistrationServiceImpl(system: ActorSystem[_]) extends UserRegistrati
     convertError(response)
   }
 
+  override def postPost(request: PostPostRequest): Future[Post] = {
+    log.info(s"Posting for user with email {}", request.author)
+    val post: socialmedia.core.user.Post = socialmedia.core.user.Post(request.content, request.image, DateTime.now.toString(), request.author)
+    val entityRef = sharding.entityRefFor(UserRegistration.EntityKey, request.author.hashCode.toString)
+    val reply: Future[socialmedia.core.user.Post] = entityRef.askWithStatus(UserRegistration.PostPost(request.author, post, _))
+    val response: Future[socialmedia.proto.Post] = reply.map(u => socialmedia.proto.Post(post.content, post.image, post.date, post.author))
+    convertError(response)
+  }
+
   private def convertError[T](response: Future[T]): Future[T] = {
     response.recoverWith {
       case _: TimeoutException =>
         Future.failed(
-          new GrpcServiceException(
-            Status.UNAVAILABLE.withDescription("Operation timed out")))
+        new GrpcServiceException(
+        Status.UNAVAILABLE.withDescription("Operation timed out")))
       case exc =>
         Future.failed(
-          new GrpcServiceException(
-            Status.INVALID_ARGUMENT.withDescription(exc.getMessage)))
+        new GrpcServiceException(
+        Status.INVALID_ARGUMENT.withDescription(exc.getMessage)))
     }
   }
+
 }
